59735d41fcd53ba0eb56e174665f5c75f86b61f1
paolaconde06 <a374460@uach.mx>
2025-10-01 08:18:12 -0700

    Create programa1.py

---
+++ programa1.py
@@ -0,0 +1,5 @@
+# Este es un script de python de prueba para el repositorio 
+def sañudo(nombre):
+  return "Hola" Nombre
+#se invoca a la funcin saludo para saludar a alguien llamado Paola 
+saludo ("Paola")
